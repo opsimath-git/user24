2b9dda9df109ccd0541bac0dee48fd8b9d50d61a
opsimath <opsimath2@naver.com>
2021-07-28 13:44:25 +0900

    2change

--- 3.vnet.tf
+++ 3.vnet.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network" "user24-vnet" {
         name                    = var.vnet
-        address_space           = ["24.0.0.0/16","192.168.0.0/16"]
+        address_space           = ["24.0.0.0/16"]
         location                = var.region
         resource_group_name     = var.resource_group
 }
