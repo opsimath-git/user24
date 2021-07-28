30a7e2e4dc75d0ba17a99c706521f510e0b058a1
opsimath <opsimath2@naver.com>
2021-07-28 13:24:27 +0900

    1change

--- 11.natrule1.tf
+++ 11.natrule1.tf
@@ -5,6 +5,6 @@ resource "azurerm_lb_nat_rule" "natrule1" {
   protocol                       = "Tcp"
   frontend_port                  = 50001	   ### ssh -i ~/.ssh/id_rsa username@LB_VIP -p 50001
   backend_port                   = 22
-  frontend_ip_configuration_name = "davidPublicIPAddress"
+  frontend_ip_configuration_name = "user24PublicIPAddress"
 }
 
